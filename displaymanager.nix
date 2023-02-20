93bbf14f8b02084ef8aa7bece84b3c13e53088f1
Daniel Shiposha <dshiposha@usetech.com>
2023-02-20 23:17:53 +0100

    fix: sddm theme package

--- displaymanager.nix
+++ displaymanager.nix
@@ -1,10 +1,12 @@
-resolution: {pkgs, ...}: {
+resolution: {pkgs, ...}:
+let theme = "simplicity"; in {
   services.xserver.displayManager.sddm = {
     enable = true;
+    inherit theme;
   };
   services.xserver.enable = true;
 
   environment.systemPackages = [
-    (pkgs.callPackage ./common/packages/sddm-theme.nix { inherit resolution; })
+    (pkgs.callPackage ./common/packages/sddm-theme/${theme}.nix { inherit resolution; })
   ];
 }
