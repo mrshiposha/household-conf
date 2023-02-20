6d4bd5863eb83762faddf08b9ad1bbdf68a5cbe0
Daniel Shiposha <dshiposha@usetech.com>
2023-02-20 22:20:22 +0100

    fix: sddm theme package

--- common/packages/sddm-theme.nix
+++ common/packages/sddm-theme.nix
@@ -1,18 +1,16 @@
-{ stdenv, fetchFromGitLab }: {
-  simplicity-sddm-theme = stdenv.mkDerivation rec {
-    pname = "simplicity-sddm-theme";
-    version = "1.0";
-    dontBuild = true;
-    installPhase = ''
-      mkdir -p $out/share/sddm/themes
-      cp -a $src/simplicity $out/share/sddm/themes
-      cp /household/common/images/1920x1080/login-screen.jpg $out/share/sddm/themes/simplicity/images/background.jpg
-    '';
-    src = fetchFromGitLab {
-      owner = "isseigx";
-      repo = pname;
-      rev = version;
-      sha256 = "1g528qqxjzgfj3w27csr3lqjxxwbj3h6rj45br0djrqgyirrp4kr";
-    };
+{ stdenv, fetchFromGitLab }: stdenv.mkDerivation rec {
+  pname = "simplicity-sddm-theme";
+  version = "1.0";
+  dontBuild = true;
+  installPhase = ''
+    mkdir -p $out/share/sddm/themes
+    cp -a $src/simplicity $out/share/sddm/themes
+    cp /household/common/images/1920x1080/login-screen.jpg $out/share/sddm/themes/simplicity/images/background.jpg
+  '';
+  src = fetchFromGitLab {
+    owner = "isseigx";
+    repo = pname;
+    rev = version;
+    sha256 = "1g528qqxjzgfj3w27csr3lqjxxwbj3h6rj45br0djrqgyirrp4kr";
   };
 }
