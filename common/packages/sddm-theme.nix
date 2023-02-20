cd63ee6e70eeca30ec04c34ca705afc4587b7670
Daniel Shiposha <dshiposha@usetech.com>
2023-02-20 22:41:12 +0100

    fix: sddm theme package

--- common/packages/sddm-theme.nix
+++ common/packages/sddm-theme.nix
@@ -4,7 +4,7 @@
   dontBuild = true;
   installPhase = let login-screen = ../images/${resolution}/login-screen.jpg; in ''
     mkdir -p $out/share/sddm/themes
-    cp -a $src/simplicity $out/share/sddm/themes
+    cp -r $src/simplicity $out/share/sddm/themes
     ls -la $out/share/sddm/themes/simplicity
     ls -la $out/share/sddm/themes/simplicity/images
     rm $out/share/sddm/themes/simplicity/images/background.jpg
